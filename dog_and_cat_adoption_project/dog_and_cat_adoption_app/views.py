82bece9922dd233a9dfa122b6fc435c2253bb08d
Apple-supaporn <spk.apple@gmail.com>
2023-11-02 22:54:57 -0700

    add .env and .gitignore and set SECRET_KEY

--- dog_and_cat_adoption_project/dog_and_cat_adoption_app/views.py
+++ dog_and_cat_adoption_project/dog_and_cat_adoption_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Pet
+#from django.http import HttpResponse
+
+
+
+
+
+
+
+
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'pets/home.html')
+
+def about(request):
+    return render(request, 'pets/about.html')  
+
+
+def pets(request):
+    pets = Pet.object.all()
+    return render(request, 'pets/pets_index.html', {'pets' : pets}) 
