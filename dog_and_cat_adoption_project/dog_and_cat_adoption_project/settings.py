947dfd6abd7ec1ee1a2a05f11e447e77dd9a2fc3
Apple-supaporn <spk.apple@gmail.com>
2023-11-09 13:55:07 -0800

    move both line under DATABASES

--- dog_and_cat_adoption_project/dog_and_cat_adoption_project/settings.py
+++ dog_and_cat_adoption_project/dog_and_cat_adoption_project/settings.py
@@ -81,8 +81,6 @@ WSGI_APPLICATION = 'dog_and_cat_adoption_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-DATABASES['default'].update(db_from_env)
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -90,6 +88,7 @@ DATABASES = {
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
