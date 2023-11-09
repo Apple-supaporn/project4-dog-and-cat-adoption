40e1ad418567596a433b4564027e6ef9cd46a65a
Apple-supaporn <spk.apple@gmail.com>
2023-11-09 12:22:06 -0800

    fix database config by move to under DATABASES =

--- dog_and_cat_adoption_project/dog_and_cat_adoption_project/settings.py
+++ dog_and_cat_adoption_project/dog_and_cat_adoption_project/settings.py
@@ -80,8 +80,6 @@ WSGI_APPLICATION = 'dog_and_cat_adoption_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 DATABASES = {
     'default': {
@@ -90,6 +88,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
